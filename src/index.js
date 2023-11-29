editor-js/link

feat: add support for custom fetch method

src/index.js
@@ -65,7 +65,8 @@ export default class LinkTool {
      * Tool's initial config
      */
     this.config = {
-      endpoint: config.endpoint || ''
+      endpoint: config.endpoint || '',
+      fetcher: config.fetcher || undefined
     };
 
     this.nodes = {
@@ -347,13 +348,18 @@ export default class LinkTool {
     this.data = { link: url };
 
     try {
-      const response = await (ajax.get({
-        url: this.config.endpoint,
-        data: {
-          url
-        }
-      }));
+      let response;
 
+      if (this.config.fetcher && typeof this.config.fetcher.fetchLinkDataForUrl === 'function') {
+        response = await (this.config.fetcher.fetchLinkDataForUrl(url));
+      } else {
+        response = await (ajax.get({
+          url: this.config.endpoint,
+          data: {
+            url
+          }
+        }));
+      }
       this.onFetch(response);
     } catch (error) {
       this.fetchingFailed('Haven\'t received data from server');

boundstate/editorjs-link

src/index.js
@@ -84,6 +84,7 @@ export default class LinkTool {
     this.config = {
       endpoint: config.endpoint || '',
       headers: config.headers || {},
+      fetcher: config.fetcher || undefined,
     };
 
     this.nodes = {
@@ -391,13 +392,20 @@ export default class LinkTool {
     this.data = { link: url };
 
     try {
-      const { body } = await (ajax.get({
-        url: this.config.endpoint,
-        headers: this.config.headers,
-        data: {
-          url,
-        },
-      }));
+      let body;
+
+      if (this.config.fetcher && typeof this.config.fetcher.fetchLinkDataForUrl === 'function') {
+        body = await (this.config.fetcher.fetchLinkDataForUrl(url));
+      } else {
+        const response = await (ajax.get({
+          url: this.config.endpoint,
+          headers: this.config.headers,
+          data: {
+            url,
+          },
+        }));
+        body = response.body;
+      }
 
       this.onFetch(body);
     } catch (error) {